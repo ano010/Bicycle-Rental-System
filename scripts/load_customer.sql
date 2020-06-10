02436610f193f6bacfd2f24fe8e34cb5d0cf3495
Anojan <anojan010@gmail.com>
2020-06-10 11:02:44 +0530

    scripts created for dumping sample data

---
+++ scripts/load_customer.sql
@@ -0,0 +1,7 @@
+USE brs;
+
+INSERT INTO customer(user_nic, registrationDate, lastRentalDate) VALUES
+('962112293V', '2000-01-01', '2020-01-01'),
+('971112293V', '2001-01-01', '2020-02-01'),
+('981112293V', '2010-01-01', NULL),
+('991112293V', '2000-05-01', NULL);
---
+++ scripts/load_payment_status.sql
@@ -0,0 +1,5 @@
+USE brs;
+
+INSERT INTO payment_status(id, status, description) VALUES
+(1, 'PAID', NULL),
+(2, 'NOT PAID', NULL);
